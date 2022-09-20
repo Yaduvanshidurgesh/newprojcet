7065d516ffd8a0601e21b046c381950c87704cd3
Yaduvanshidurgesh <104846676+Yaduvanshidurgesh@users.noreply.github.com>
2022-09-20 22:43:23 +0530

    Update Test.java

--- src/Testcases/Test.java
+++ src/Testcases/Test.java
@@ -22,6 +22,15 @@ public class Test {
         }
          driver.quit();
     }
+	
+	try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+         driver.quit();
+    }
 		 
 	}
 
