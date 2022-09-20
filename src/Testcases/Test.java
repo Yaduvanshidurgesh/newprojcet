576c2c3b96b63e3e9e5141c07134430cc1d41439
Yaduvanshidurgesh <durgeshyaduvanshi108@gmail.com>
2022-09-20 21:59:16 +0530

    My project testfiles

---
+++ src/Testcases/Test.java
@@ -0,0 +1,28 @@
+package Testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class Test {
+
+	public static WebDriver driver;
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Driversfiles\\chromedriver.exe");
+
+		driver = new ChromeDriver();
+        driver.get("https://www.syncfusion.com/");
+         System.out.println("Successfully opened the website www.Syncfusion.com"); 
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+         driver.quit();
+    }
+		 
+	}
+
+
